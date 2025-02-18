5b5ffa51713c60698983ecf2b3a9b751a51331b3
s13g <ayflix0@gmail.com>
2025-02-18 19:39:21 +0100

    finished templating

---
+++ db/schema.rb
@@ -0,0 +1,9 @@
+ActiveRecord::Schema[7.1].define(version: 2025_02_18_144516) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
