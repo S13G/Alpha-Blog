67734cd7ef41ff43c90fa542b484b01ec01aa6fc
s13g <ayflix0@gmail.com>
2025-02-19 14:47:09 +0100

    Add user profile views, Gravatar integration, and refactor articles table
    Implemented views for user profile display and editing, including Gravatar support for profile pictures. Refactored the articles table into a partial for reuse across views. Updated functionality in `UsersController` to handle user CRUD operations and added conditional button labels for user form submissions.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,40 @@
 class UsersController < ApplicationController
+  # before_action:require_login, except: [:new, :create]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def new
     @user = User.new
+  end
+
+  def show
+    @articles = @user.articles
+  end
+
+  def edit
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to articles_path, notice: "Welcome to the Alpha Blog #{user.username}, you have successfully signed up"
+      redirect_to articles_path, :notice => "Welcome to the Alpha Blog #{user.username}, you have successfully signed up"
     else
       render :new
     end
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, :notice => "Your account has been updated successfully"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    redirect_to root_path, :notice => "Account and all associated articles successfully deleted"
   end
 
   private
@@ -18,4 +42,13 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # def require_login
+  # unless logged_in?
+  #   redirect_to login_path, :alert => "You must be logged in to perform that action"
+  # end
 end
